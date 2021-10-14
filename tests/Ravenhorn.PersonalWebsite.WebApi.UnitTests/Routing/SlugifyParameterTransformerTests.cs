251a53eebf935f9503d9aa0fb08d9cd53a2dbb4e
Alexander Trishin <Alexander_Trishin@mail.ru>
2021-10-14 14:09:32 +0300

    Add unit tests for web api project

---
+++ tests/Ravenhorn.PersonalWebsite.WebApi.UnitTests/Routing/SlugifyParameterTransformerTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Ravenhorn.PersonalWebsite.WebApi.Routing;
+using Xunit;
+
+namespace Ravenhorn.PersonalWebsite.WebApi.UnitTests.Routing
+{
+    public class SlugifyParameterTransformerTests
+    {
+        [Fact]
+        public void TransformOutbound_ShouldReturnNull_WhenValueIsNull()
+        {
+            var transformer = new SlugifyParameterTransformer();
+
+            var actual = transformer.TransformOutbound(null);
+
+            actual.Should().BeNull();
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("Home", "home")]
+        [InlineData("ContactMe", "contact-me")]
+        [InlineData("Account/SignUp", "account/sign-up")]
+        public void TransformOutbound_ShouldTransformToSnakeCase_WhenStringValueProvided(string input, string output)
+        {
+            var transformer = new SlugifyParameterTransformer();
+
+            var actual = transformer.TransformOutbound(input);
+
+            actual.Should().Be(output);
+        }
+    }
+}
